eb9ce71f2b22149484c034967947f6b4b1cd6304
G-WS <3355139577@qq.com>
2022-08-02 13:28:30 +0800

    20220802

--- app/src/main/java/com/example/activitytest/FirstActivity.java
+++ app/src/main/java/com/example/activitytest/FirstActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -24,30 +25,45 @@ public class FirstActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                    //Toast显示
 //                Toast.makeText(FirstActivity.this, "you click Button1", Toast.LENGTH_SHORT).show();
+        //跳转到第二个activity
 //                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+                //使用隐式intent启动活动
                 Intent intent = new Intent("com/example/activitytest.ACTION_START");
                 intent.addCategory("com/example/activitytest.MY_CATEGORY");
                 Log.d("data", "onClick: ");
                 startActivity(intent);
             }
         });
+//        Button button1 = (Button) findViewById(R.id.button_1);
+//        button1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+////                Intent intent = new Intent(Intent.ACTION_VIEW);
+////                intent.setData(Uri.parse("http://www.baidu.com"));
+////                startActivity(intent);
+//                Intent intent = new Intent(Intent.ACTION_DIAL);
+//                intent.setData(Uri.parse("tel:10086"));
+//                startActivity(intent);
+//            }
+//        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main,menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.add_item:
-                Toast.makeText(this,"you click Add",Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "you click Add", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.remove_item:
-                Toast.makeText(this,"you click Remove",Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "you click Remove", Toast.LENGTH_SHORT).show();
                 break;
             default:
         }
